301fc10ddd2d104cbd067575b72fb6e84cd0a1a7
Alexis Aguilar <alexmerona@gmail.com>
2023-04-09 15:19:25 -0400

    input tag to shadcn input component

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -51,10 +51,9 @@ export const Hero = () => {
           </Select>
 
           {/* Markings */}
-          <input
+          <Input
             type="text"
             placeholder="Markings e.g. 'Spots', 'Black socks'"
-            className="bg-base-100 flex h-10 rounded-md border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           />
         </div>
       </div>
