44b731ce6816551402324853a594d74867f7356e
Alexis Aguilar <alexmerona@gmail.com>
2023-05-29 17:46:26 -0400

    refactor: turn the STINKY hydration error off thats angry about next-themes

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -30,7 +30,10 @@ const Header = ({ loggingIn }: HeaderProps) => {
   const handleLogin = () => push(`/login?callbackUrl=${asPath}`);
 
   return (
-    <header className="suppressHydrationWarning sticky top-0 z-40 w-full items-center justify-between backdrop-blur">
+    <header
+      suppressHydrationWarning
+      className="sticky top-0 z-40 w-full items-center justify-between backdrop-blur"
+    >
       <div className="container flex h-14 items-center ">
         <div className="mr-4 flex">
           <Link className="mr-6 flex items-center space-x-2" href="/">
