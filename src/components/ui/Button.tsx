c9de30f09fb0213dc87beddf8674ac0dad9cb533
Alexis Aguilar <alexmerona@gmail.com>
2023-05-29 17:22:16 -0400

    style: focus styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,7 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "~/utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-accent text-background dark:text-foreground",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:opacity-50 disabled:pointer-events-none ring-offset-transparent bg-accent text-background dark:text-foreground",
   {
     variants: {
       variant: {
